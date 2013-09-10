3654f9a300fa2e08d17aa76c7a186b1f71e978c8
Jonas Wagner <jonas.wagner@epfl.ch>
2013-09-10 17:16:14 +0200

    Remove timers from the Solo class.

--- robotium-solo/src/main/java/com/jayway/android/robotium/solo/Waiter.java
+++ robotium-solo/src/main/java/com/jayway/android/robotium/solo/Waiter.java
@@ -329,7 +329,7 @@ class Waiter {
 			}
 			sleeper.sleep();
 
-			WebElement webElementToReturn = searcher.searchForWebElement(by, minimumNumberOfMatches, timeout, scroll); 
+			WebElement webElementToReturn = searcher.searchForWebElement(by, minimumNumberOfMatches, scroll); 
 
 			if(webElementToReturn != null)
 				return webElementToReturn;
@@ -461,7 +461,7 @@ class Waiter {
 			if(!hardStoppage)
 				timeout = 0;
 
-			final T textViewToReturn = searcher.searchFor(classToFilterBy, text, expectedMinimumNumberOfMatches, timeout, scroll, onlyVisible);
+			final T textViewToReturn = searcher.searchFor(classToFilterBy, text, expectedMinimumNumberOfMatches, scroll, onlyVisible);
 
 			if (textViewToReturn != null ){
 				return textViewToReturn;
